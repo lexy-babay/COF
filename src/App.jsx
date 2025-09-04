54c05efd87be9d79c86e1fb81eee98c1d622da78
lexy-babay <mesomaonubeze@gmail.com>
2025-09-04 19:00:42 +0100

    cof

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='h-[100vh] bg-amber-300'></div>
+    </>
+  )
+}
+
+export default App
